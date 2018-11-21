063067b549be5a3c272bb0a1d0ad455ab37b8d79
Prateek Ojha <prateek479@gmail.com>
2018-11-21 14:50:09 +0000

    adding import helper for openSettingsMenu

--- transforms/acceptance/index.js
+++ transforms/acceptance/index.js
@@ -1,7 +1,7 @@
 const { getParser } = require('codemod-cli').jscodeshift;
 const { exampleTransform } = require('../../utils/acceptance/example-transform');
 const { replaceIdentifier } = require('../../utils/identifier');
-const { removeImport } = require('../../utils/imports');
+const { removeImport, addImport } = require('../../utils/imports');
 
 module.exports = function transformer(file, api) {
   const j = getParser(api);
@@ -15,5 +15,9 @@ module.exports = function transformer(file, api) {
   // containerLookup to this.owner.lookup
   code = removeImport(j, code, 'embercom/lib/container-lookup');
   code = replaceIdentifier(j, code, 'containerLookup', 'this.owner.lookup');
+
+  // import path for openSettingsMenu
+  code = addImport(j, code, 'openSettingsMenu', 'embercom/tests/helpers/settings-menu');
+
   return code;
 };
