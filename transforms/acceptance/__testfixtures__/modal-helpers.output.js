ec6ecab0d30b84f3ae48fa7e990cb66fddf9d0d3
Prateek Ojha <prateek479@gmail.com>
2018-11-28 12:18:37 +0000

    adding modal helper

--- transforms/acceptance/__testfixtures__/modal-helpers.output.js
+++ transforms/acceptance/__testfixtures__/modal-helpers.output.js
@@ -1,10 +1,11 @@
 import { setupApplicationTest } from 'ember-qunit';
+import { assertModalHeader, closeModal } from "embercom/tests/helpers/modal";
 import { visit } from '@ember/test-helpers';
 
-module('some module', function(hooks) {
+module('some module', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('test', async function(assert) {
+  test('test', async function (assert) {
     visit('/some/route');
     await closeModal();
     assertModalHeader(assert, 'Delete Intercom workspace');
